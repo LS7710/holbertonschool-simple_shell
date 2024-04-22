6f8ef98d9fd12b8ed2a04c5adb27d1a7e9744b8c
jm <jjmendezrodriguez@yahoo.com>
2024-04-22 11:14:32 -0400

    fix main

--- main.c
+++ main.c
@@ -1,22 +1,63 @@
-#include "librery.h"
-/*
- * main - make the shell work
- */
+#include "library.h"
+
 int main(void)
 {
-	char cmd[128];
+    char cmd[128];
+    int status;
 
-	while(1)
-	{
-		printf("$ ");
-		getline("%s", cmd);
+    while (1)
+    {
+        printf("$ ");
+        if (fgets(cmd, sizeof(cmd), stdin) == NULL)
+        {
+            if (feof(stdin))
+            {
+                printf("\n");
+                break;
+            }
+            else
+            {
+                perror("fgets");
+                continue;
+            }
+        }
 
-	if (strcmp(cmd, "exit") == 0)
-		break;
-	else
-	{
-		
-	}	
 
-return (0);	
+        cmd[strcspn(cmd, "\n")] = '\0';
+
+        if (strcmp(cmd, "exit") == 0)
+        {
+            break;
+        }
+        else
+        {
+            pid_t pid = fork();
+            if (pid == -1)
+            {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            else if (pid == 0)
+            {
+              
+                if (execlp(cmd, cmd, NULL) == -1)
+                {
+                    perror("execlp");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+               
+                if (waitpid(pid, &status, 0) == -1)
+                {
+                    perror("waitpid");
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+
+    return 0;
 }
+
