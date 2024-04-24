5a1ab6c916dbffcb45d8824bf5560b9143a7ab9d
luissoto7710 <luissoto7710@gmail.com>
2024-04-24 16:11:53 -0400

    My Commit

--- shell.c
+++ shell.c
@@ -1,36 +1,41 @@
 #include "shell.h"
 
+
+
 void execute_command(char *cmd)
 {
-	char *argv[10];
-	int i = 0;
-	pid_t pid;
-	int j;
+    char *argv[10];
+    int i = 0;
+    pid_t pid;
+    int j;
 
-	argv[i] = strtok(cmd, " \n");
-	while (argv[i] != NULL && i < 9) {
-	argv[++i] = strtok(NULL, " \n");
-	}	
+    argv[i] = strtok(cmd, " \n");
+    while (argv[i] != NULL && i < 9) {
+        argv[++i] = strtok(NULL, " \n");
+    }
 
 
-	for (j = 0; j <= i; j++) {
-	printf("argv[%d]: %s\n", j, argv[j]);
-	}
 
-	if (argv[0] != NULL) {
-	if (strcmp(argv[0], "exit") == 0)
-	exit(0);
+    for (j = 0; j <= i; j++) {
+        printf("argv[%d]: %s\n", j, argv[j]);
+    }
 
-	pid = fork();
-	if (pid == 0) {
-	if (execvp(argv[0], argv) == -1) {
-		perror("hsh");
-		}
-	exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-	perror("hsh");
-	} else {
-	wait(NULL);
-	}
-	}
+
+    if (argv[0] != NULL) {
+        if (strcmp(argv[0], "exit") == 0)
+                exit(0);
+
+        pid = fork();
+        if (pid == 0) {
+            if (execvp(argv[0], argv) == -1) {
+                perror("hsh");
+            }
+            exit(EXIT_FAILURE);
+        } else if (pid < 0) {
+            perror("hsh");
+        } else {
+            wait(NULL);
+        }
+    }
 }
+
