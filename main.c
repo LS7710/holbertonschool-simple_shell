2086c5c6bcc3347f4c368d5d757c64ce131392d0
luissoto7710 <luissoto7710@gmail.com>
2024-04-24 13:07:23 -0400

    My Commit

--- main.c
+++ main.c
@@ -7,14 +7,6 @@ int main(void)
     ssize_t read;
     char *prompt = "$ ";
 
-
-    printf(" _       __     __                             __  \n"
-           "| |     / /__  / /________  ____ ___  ___     / /  \n"
-           "| | /| / / _ \\/ / ___/ __ \\/ __ `__ \\/ _ \\   / /   \n"
-           "| |/ |/ /  __/ / /__/ /_/ / / / / / /  __/  /_/    \n"
-           "|__/|__/\\___/_/\\___/\\____/_/ /_/ /_/\\___(_)(_)     \n"
-           "                                                    \n");
-
     while (printf("%s", prompt), (read = getline(&line, &len, stdin)) != -1)
     {
         execute_command(line);
