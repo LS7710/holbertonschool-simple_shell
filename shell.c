e5f69d15dd0ea9908b691d22a097747225f76747
luissoto7710 <luissoto7710@gmail.com>
2024-04-24 16:33:23 -0400

    My Commit

--- shell.c
+++ shell.c
@@ -1,17 +1,19 @@
 #include "shell.h"
+#include <sys/wait.h>
 
 void execute_command(char *cmd)
 {
     char *argv[10];
     int i = 0;
     pid_t pid;
+    int status;
 
     argv[i] = strtok(cmd, " \n");
     while (argv[i] != NULL && i < 9) {
         argv[++i] = strtok(NULL, " \n");
     }
 
-    if (argv[0] != NULL) {
+    if (argv[0]) {
         if (strcmp(argv[0], "exit") == 0) {
             free(cmd);
             exit(0);
@@ -26,7 +28,11 @@ void execute_command(char *cmd)
         } else if (pid < 0) {
             perror("hsh");
         } else {
-            wait(NULL);
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status)) {
+                int exit_status = WEXITSTATUS(status);
+                printf("Command exited with status: %d\n", exit_status);
+            }
         }
     }
 }
