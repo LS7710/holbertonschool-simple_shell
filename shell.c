b8a485d1277b030f2b59d3104f5c2f5b2257e445
luissoto7710 <luissoto7710@gmail.com>
2024-04-25 11:56:41 -0400

    My Commit

--- shell.c
+++ shell.c
@@ -1,33 +1,43 @@
 #include "shell.h"
+#define MAX_ARGS 124
 
 int execute_command(char *cmd) {
-    char *argv[10];
-    int i = 0;
+    char *argv[MAX_ARGS];
+    int argc = 0;
     pid_t pid;
     int status;
+    int i;
+    int j;
 
-    argv[i] = strtok(cmd, " \t");
-    while (argv[i] != NULL && i < 9) {
-        argv[++i] = strtok(NULL, " \t");
+    char *token = strtok(cmd, " \n\t");
+    while (token != NULL && argc < MAX_ARGS - 1) {
+        argv[argc++] = strdup(token);
+        token = strtok(NULL, " \n\t");
     }
-    argv[i] = NULL;
+    argv[argc] = NULL;
 
-    if (argv[0]) {
-        pid = fork();
-        if (pid == 0) {
-            execvp(argv[0], argv);
-
-            fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-            exit(127);
-	} else if (pid < 0) {
-            perror("fork failed");
-            return EXIT_FAILURE;
-        } else {
-    		waitpid(pid, &status, 0);
-    		if (WIFEXITED(status)) {
-                return WEXITSTATUS(status);
-    		}
-        }
+    if (argv[0] == NULL) {
+        return 0;
     }
-    return EXIT_SUCCESS;
+
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        for (j = 0; j < argc; j++) free(argv[j]);
+        return EXIT_FAILURE;
+    } else if (pid == 0) {
+        execvp(argv[0], argv);
+        fprintf(stderr, "Failed to execute '%s'\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+
+    waitpid(pid, &status, 0);
+    for (i = 0; i < argc; i++) free(argv[i]);
+
+    if (WIFEXITED(status)) {
+        return WEXITSTATUS(status);
+    }
+
+    return 1;
 }
