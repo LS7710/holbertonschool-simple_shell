5ed8e1045c7b771b237e3a950dc75bdd7fd41ff9
luissoto7710 <luissoto7710@gmail.com>
2024-04-25 13:45:28 -0400

    My Commit

--- shell.c
+++ shell.c
@@ -3,37 +3,41 @@
 
 int execute_command(char *cmd) {
     char *argv[10];
-    int argc = 0;
+    int i = 0;
     pid_t pid;
     int status;
+    int j;
+    int k;
+    int f;
 
-    char *token = strtok(cmd, " \n\t");
-    while (token != NULL && argc < 10 - 1) {
-        argv[argc++] = token;
-        token = strtok(NULL, " \n\t");
+
+    char *token = strtok(cmd, " ");
+    while (token != NULL && i < 9) {
+        argv[i++] = strdup(token);
+        token = strtok(NULL, " ");
     }
-    argv[argc] = NULL;
+    argv[i] = NULL;
 
     if (argv[0] == NULL) {
+
+        for (j = 0; j < i; j++) free(argv[j]);
         return 0;
     }
 
     pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        return EXIT_FAILURE;
-    } else if (pid == 0) {
-
+    if (pid == 0) {
         execvp(argv[0], argv);
-        fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-        exit(127);
-    }
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        waitpid(pid, &status, 0);
 
-
-    waitpid(pid, &status, 0);
-    if (WIFEXITED(status)) {
+        for (f = 0; f < i; f++) free(argv[f]);
         return WEXITSTATUS(status);
-    }
+    } else {
+        perror("fork");
 
-    return EXIT_SUCCESS;
+        for (k = 0; k < i; k++) free(argv[k]);
+        return EXIT_FAILURE;
+    }
 }
+
