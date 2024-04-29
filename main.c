512c1a8e5f1bb186f49fd569bd0669961cd99058
luissoto7710 <luissoto7710@gmail.com>
2024-04-28 23:34:54 -0400

    My Commit

--- main.c
+++ main.c
@@ -1,29 +1,45 @@
 #include <unistd.h>
 #include "shell.h"
 
-void remove_newline(char* line) {
-    int i = 0;
-    while (line[i] != '\0') {
-        if (line[i] == '\n') {
-            line[i] = '\0';
-            break;
-        }
-        i++;
-    }
+/**
+ * remove_newline - removes new lines
+ * @line: the line from stdo
+ *
+ * Return: void
+ */
+void remove_newline(char *line)
+{
+	int i = 0;
+
+	while (line[i] != '\0')
+	{
+		if (line[i] == '\n')
+		{
+			line[i] = '\0';
+			break;
+		}
+		i++;
+	}
 }
 
-int main(void) {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int last_status = 0;
+/**
+ * main - main function for our shell
+ *
+ * Return: last_status
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int last_status = 0;
 
-    while ((read = getline(&line, &len, stdin)) != -1) {
-        remove_newline(line);
+	while ((read = getline(&line, &len, stdin)) != -1)
+	{
+		remove_newline(line);
 
-	last_status = execute_command(line);
-    }
-
-    free(line);
-    return last_status;
+		last_status = execute_command(line);
+	}
+	free(line);
+	return (last_status);
 }
